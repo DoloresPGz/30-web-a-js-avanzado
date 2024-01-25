0a71dc79a34f07ba1319f76c1e554fb07ee70426
DoloresPGz <dolores.pgz@gmail.com>
2024-01-24 18:30:45 -0600

    agregamos picocolors

--- 1-1-Node/index.js
+++ 1-1-Node/index.js
@@ -1,4 +1,11 @@
+//Importamos picocolors usando la sintaxis de common js (CJS)
+const pc = require('picocolors')
+
 console.log("Hola mundo!")
+
+console.log(
+    pc.bgYellow(pc.blue(`How are ${pc.underline(`you`)} doing?`))
+)
 
 //Ciclo infinito! Lo detengo usando ctl + c
 // while(true){
