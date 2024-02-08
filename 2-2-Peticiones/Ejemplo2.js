8d3903c8ddb0270b9a46eff2ee365af356080f47
hiromiakitao@gmail.com <hiromiakitao@gmail.com>
2024-02-07 20:05:30 -0600

    ejercicio paises

--- 2-2-Peticiones/Ejemplo2.js
+++ 2-2-Peticiones/Ejemplo2.js
@@ -1,9 +1,23 @@
-const obtenerPaises = () => {
+const obtenerTodosLosPaises = () => {
     const API_URL = 'https://restcountries.com/v3.1/all';
 
     fetch(API_URL)
         .then(respuesta => respuesta.json())
-        .then(info => console.log(info.length))
+        .then(info => {
+            info.forEach(pais => {
+                console.log(pais.name.common)
+            });
+        })
 }
 
-obtenerPaises()
+const buscadorPaises = (query) => {
+    const API_URL = `https://restcountries.com/v3.1/name/${query}`
+
+    fetch(API_URL)
+        .then(respuesta => respuesta.json())
+        .then(info => info.forEach(pais => console.log(pais.name.common)))
+
+}
+
+// obtenerTodosLosPaises()
+buscadorPaises('arg')
